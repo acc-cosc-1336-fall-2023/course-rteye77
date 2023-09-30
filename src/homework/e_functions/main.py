853d682d00fc87e567534fa126ad48ad43d5b332
rteye77 <143046411+rteye77@users.noreply.github.com>
2023-09-30 18:48:07 +0000

    home work 4

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,32 @@
 #
+import value_return
+
+hour = value_return.get_hour(3800)
+minutes = value_return.get_minutes(3800)
+seconds = value_return.get_seconds(3800)
+
+add_zero_hr = True
+add_zero_mins = True
+add_zero_secs = True
+if(hour > 10):
+    add_zero_hr = False
+
+if(minutes > 10):
+    add_zero_mins = False
+
+if(seconds > 10):
+    add_zero_secs = False
+
+colon_hr = ''
+if(add_zero_hr):
+    colon_hr = '0'
+
+colon_mins = ':'
+if(add_zero_mins):
+    colon_mins = ':0'
+
+colon_secs = ':'
+if(add_zero_secs):
+    colon_secs = ':0'
+
+print(colon_hr + str(hour) + colon_mins + str(minutes) + colon_secs + str(seconds))
--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,13 @@
 #
+def get_hour(epoch_seconds):
+    return epoch_seconds//3600
+
+def get_minutes(epoch_seconds):
+    mins = epoch_seconds//60
+    mins_mod = mins % 60
+    return mins_mod
+
+def get_seconds(epoch_seconds):
+    secs = (epoch_seconds%3600) % 60
+    return secs
+
