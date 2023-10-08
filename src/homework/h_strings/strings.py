bb85c6f3f715752d707a62e8f8936b17bd5ba796
rteye77 <143046411+rteye77@users.noreply.github.com>
2023-10-08 04:31:30 +0000

    Home work 5

--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,15 @@
 #
+def get_hamming_distance(dna1,dna2):
+    dist = 0
+    for i in range(0, len(dna1)):
+        if dna1[i] != dna2[i]:
+            dist = dist + 1
+    return dist
+
+def get_dna_complement(dna):
+    complements = {'A':'T', 'C':'G', 'T':'A', 'G':'C'}
+    dna_reverse = dna[::-1]
+    complement = ""
+    for strand in dna_reverse:
+        complement += complements[strand]
+    return complement
